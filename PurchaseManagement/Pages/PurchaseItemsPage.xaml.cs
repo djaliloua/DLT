de5164a1ed2ef9e2a8cf81d306b0332860cdb356
Abdou-Djalilou Ali <djaliloua@gmail.com>
2024-04-21 14:19:15 +0200

    Scroll back the item that has been previousely clicked

--- PurchaseManagement/Pages/PurchaseItemsPage.xaml.cs
+++ PurchaseManagement/Pages/PurchaseItemsPage.xaml.cs
@@ -15,6 +15,16 @@ public partial class PurchaseItemsPage : ContentPage
         if (RegisterViewModels.PurchaseItemsViewModel.Purchases is Purchases p)
         {
             await RegisterViewModels.PurchaseItemsViewModel.LoadPurchaseItemsAsync(p.Purchase_Id);
+            if(RegisterViewModels.PurchaseItemDetailsViewModel.PurchaseDetails is Purchase_Items purch)
+            {
+                Dispatcher.Dispatch(() =>
+                {
+                    listview.ScrollTo(RegisterViewModels.PurchaseItemsViewModel.Purchase_Items.FirstOrDefault(item => item.Item_Id == purch.Item_Id), 
+                        ScrollToPosition.End, false);
+                });
+                
+            }
+            
         }
     }
 }
