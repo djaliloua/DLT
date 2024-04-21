ec16145f355d2a879c2d67e6b4771d6ba85db141
Abdou-Djalilou Ali <djaliloua@gmail.com>
2024-04-21 14:35:05 +0200

    Fix Scroll back not to laod data once downloaded

--- PurchaseManagement/Pages/PurchaseItemsPage.xaml.cs
+++ PurchaseManagement/Pages/PurchaseItemsPage.xaml.cs
@@ -12,19 +12,20 @@ public partial class PurchaseItemsPage : ContentPage
 	}
     private async void PurchaseItemsPage_NavigatedTo(object sender, NavigatedToEventArgs e)
     {
-        if (RegisterViewModels.PurchaseItemsViewModel.Purchases is Purchases p)
+        if (RegisterViewModels.PurchaseItemsViewModel.Purchases is Purchases p && RegisterViewModels.PurchaseItemDetailsViewModel.PurchaseDetails is null)
         {
             await RegisterViewModels.PurchaseItemsViewModel.LoadPurchaseItemsAsync(p.Purchase_Id);
-            if(RegisterViewModels.PurchaseItemDetailsViewModel.PurchaseDetails is Purchase_Items purch)
+            return;
+        }
+        if (RegisterViewModels.PurchaseItemDetailsViewModel.PurchaseDetails is Purchase_Items purch)
+        {
+            await Task.Delay(10);
+            Dispatcher.Dispatch(() =>
             {
-                Dispatcher.Dispatch(() =>
-                {
-                    listview.ScrollTo(RegisterViewModels.PurchaseItemsViewModel.Purchase_Items.FirstOrDefault(item => item.Item_Id == purch.Item_Id), 
-                        ScrollToPosition.End, false);
-                });
-                
-            }
-            
+                listview.ScrollTo(RegisterViewModels.PurchaseItemsViewModel.Purchase_Items.FirstOrDefault(item => item.Item_Id == purch.Item_Id),
+                    ScrollToPosition.End, false);
+            });
+
         }
     }
 }
