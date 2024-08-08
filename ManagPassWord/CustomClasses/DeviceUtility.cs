ee2bb63a58e201c945b69c526a5c221bd962d0ba
Abdou-Djalilou Ali <djaliloua@gmail.com>
2024-08-08 19:47:45 +0200

    - added device properties

--- ManagPassWord/CustomClasses/DeviceUtility.cs
+++ ManagPassWord/CustomClasses/DeviceUtility.cs
@@ -8,6 +8,14 @@
             DeviceType.Virtual => true,
             _ => false
         };
+        public static bool IsAndroid => DeviceInfo.Current.Platform == DevicePlatform.Android;
+        public static bool IsDeskTop => DeviceInfo.Current.Platform == DevicePlatform.WinUI;
+        public static string DeviceModel => DeviceInfo.Current.Model;
+        public static string DeviceManufacturer => DeviceInfo.Current.Manufacturer;
+        public static string DeviceName => DeviceInfo.Current.Name;
+        public static DeviceIdiom DeviceIdiom => DeviceInfo.Current.Idiom;
+        public static DevicePlatform DevicePlatform => DeviceInfo.Current.Platform;
+        public static string DeviceVersion => DeviceInfo.Current.VersionString;
 
     }
 }
