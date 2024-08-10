32196f3aedf21aa9fbb584365b94fbad59791447
Abdou-Djalilou Ali <djaliloua@gmail.com>
2024-08-10 13:26:49 +0200

    documentatio

--- PurchaseManagement/MVVM/ViewModels/AboutViewModel.cs
+++ PurchaseManagement/MVVM/ViewModels/AboutViewModel.cs
@@ -7,12 +7,14 @@ namespace PurchaseManagement.MVVM.ViewModels
 {
     public class AboutViewModel:BaseViewModel
     {
+        public string Markdown { get; } = "## Purchase Management Application(PMA)\r\n### Project description\r\nPurchase Management Application(PMA) is an application that is used to logging daily operations.\nThe app contains three main pages: Account, Purchase and Analytics pages.\r\n- Account Page: Contains estimated total daily expenses.\r\n- Analytics Page: Shows barplot of daily expenses.\r\n- Purchase Page: This page is responsible for adding products that are or will be purchased on daily basis.\r\nIt is also possible to take location where a given product has been purchased by swiping to the right on top of the product.\r\n\r\n### Technologies\r\n- MAUI\r\n- MVVM\r\n- Sqlite\r\n- Urannium\r\n- Entity framework\r\n- FingerPrint\r\n- Mapster\r\n- CommunityToolkit.Maui\r\n- CommunityToolkit.Mvvm\r\n- LiveChartsCore.SkiaSharpView.Maui\r\n- FluentValidation\r\n\r\n### Author\r\n- Software developer at MSC Technology\r\n- Abdou Djalilou Ali\r\n- djalilouagmail.com\r\n";
         public string DevName { get; set; }
         public ICommand DialCommand { get; private set; }
         public ICommand CopyCommand { get; private set; }
         public AboutViewModel()
         {
             DevName = "Abdou Djalilou Ali";
+            //Markdown = File.ReadAllText("purchase.md");
             DialCommand = new Command(On_Dial);
             CopyCommand = new Command(On_Copy);
         }
